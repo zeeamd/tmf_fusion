f42359ca56b30a01ac45f0aa6d925d11ba810774
zeeamd <35802043+zeeamd@users.noreply.github.com>
2018-10-02 17:46:22 +0530

    testing account

--- upload.sh
+++ upload.sh
@@ -3,10 +3,14 @@
 u=`cat /var/lib/jenkins/artifactory/.AU`
 p=`cat /var/lib/jenkins/artifactory/.P`
 
+echo $1
+echo $2
+
 #recursive python wheel creation
 for d in `ls -lrtd */|awk -F' ' '{print $9}'`
 do
 cd $d
+echo $1 $2
 curl -u $1:$2 -T dist/extracts-_python_version_-py2-none-any.whl "http://18.188.209.12:8081/artifactory/generic-local/"
 cd -
 done
