ef92b3f012451fec6eeac4ab5af108d689b8aaf2
root <root@ip-10-0-0-20.us-east-2.compute.internal>
2018-10-02 12:08:12 +0000

    upload to JFROG

---
+++ upload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+u=`cat /var/lib/jenkins/artifactory/.AU`
+p=`cat /var/lib/jenkins/artifactory/.P`
+
+#recursive python wheel creation
+for d in `ls -lrtd */|awk -F' ' '{print $9}'`
+do
+cd $d
+curl -u $1:$2 -T dist/*.whl "http://18.188.209.12:8081/artifactory/generic-local/"
+cd -
+done
