6dcb018c649ecc12619177adde97972c2dc57dc9
zeeamd <35802043+zeeamd@users.noreply.github.com>
2018-10-02 17:44:00 +0530

    hardcode filename whl upload curl cmd line

--- upload.sh
+++ upload.sh
@@ -7,6 +7,6 @@ p=`cat /var/lib/jenkins/artifactory/.P`
 for d in `ls -lrtd */|awk -F' ' '{print $9}'`
 do
 cd $d
-curl -u $1:$2 -T dist/*.whl "http://18.188.209.12:8081/artifactory/generic-local/"
+curl -u $1:$2 -T dist/extracts-_python_version_-py2-none-any.whl "http://18.188.209.12:8081/artifactory/generic-local/"
 cd -
 done
